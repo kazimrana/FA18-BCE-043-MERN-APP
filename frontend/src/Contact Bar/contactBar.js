e9e09a9ce628c9852611bb77446de031faaf0119
kazimrana <kazimrana66@gmail.com>
2023-05-08 18:19:07 +0500

    add more notes

--- frontend/src/Contact Bar/contactBar.js
+++ frontend/src/Contact Bar/contactBar.js
@@ -14,6 +14,8 @@ function ContactBar() {
               <button id="cb-b1"> CONTACT NOW </button>
               <button id="cb-b1"> CONTACT NOW </button>
               <button id="cb-b1"> CONTACT NOW </button>
+              <button id="cb-b1"> CONTACT NOW </button>
+              <button id="cb-b1"> CONTACT NOW </button>
             </Link>
           </div>
         </div>
